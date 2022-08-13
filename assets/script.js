8a2b2e3edb5d805b6c503c9cce1dc908fb0ce6f4
Gabe Vee <0gabevee0@gmail.com>
2022-08-12 23:02:35 -0600

    updated js file

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,25 @@
-var eventContaner = $(".description");
+var textArea = $("#text-area");
+var saveBtn = $("#save");
+//
 var displayDate = $("#currentDay");
 var today = moment().format("dddd, MMMM Do YYYY, h a");
 $("#currentDay").text(today);
+
+function renderLastTask() {
+  //   var taskDes = JSON.parse(localStorage.getItem("taskDes"));
+  $("#text-area").val(localStorage.getItem("taskDes")); //Get
+  console.log(textArea);
+  textArea.text(taskDes);
+}
+
+saveBtn.on("click", function (event) {
+  var taskDes = $("#text-area").val();
+
+  localStorage.setItem("taskDes", JSON.stringify(taskDes));
+  //   localStorage.setItem($("#text-area").val()); //Set
+
+  console.log(taskDes);
+  renderLastTask();
+});
+
+$("#note1input").val(localStorage.getItem("abc")); //Get
