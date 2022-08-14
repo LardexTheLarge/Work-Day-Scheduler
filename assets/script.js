c2937dcab76d84781a9a5c4c4db5f23ce1a38c86
Gabe Vee <0gabevee0@gmail.com>
2022-08-14 15:08:35 -0600

    added comments describing what lines of codes do

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,18 @@ var displayDate = $("#currentDay");
 var today = moment().format("dddd, MMMM Do YYYY, h a");
 $("#currentDay").text(today);
 
+//this function renders the saved values to each time slot
 function renderLastTask() {
-  //the value of timeslot gets taken from the local storage and put in the
+  //the value of the task variable gets taken from the local storage and put in the
   //correct time spot
   for (let i = 8; i <= 12; i++) {
+    //this for loop goes through 8 to 12 pm
+
     var keyValue = JSON.parse(localStorage.getItem(i));
     console.log(keyValue);
     $("#text-area-" + i).val(keyValue);
   }
+  //this for loop goes through 1 to 7pm
   for (let i = 1; i <= 7; i++) {
     var keyValue = JSON.parse(localStorage.getItem(i));
     console.log(keyValue);
@@ -22,11 +26,12 @@ function renderLastTask() {
   }
 }
 
+//this function checks the time and color codes the past, present and future
 function checkTime() {
-  //.each loops through
+  //.each loops through the element .time-block and applies the if statement to all the time-blocks
   $(".time-block").each(function () {
-    var blockId = parseInt($(this).attr("id"));
-    var currentHour = moment().format("h");
+    var blockId = parseInt($(this).attr("id")); //.this references to the .time-block
+    var currentHour = moment().format("H"); //currentHour uses military time
 
     if (currentHour > blockId) {
       $(this).addClass("past");
@@ -44,13 +49,11 @@ checkTime();
 
 //saves text area when clicked
 saveBtn.on("click", function () {
-  console.log("I was clicked");
-  //gets the id of the element this functions being applied to and splits the id
-  var slotId = this.id.split("-")[1]; //splits save - i to an array and calls the number in the id
-  console.log(slotId);
-  //task is the id of text-area- slotId and gets the value of that specific text area
-  //then sets the stringifyed task into local storage
+  //.splits save-i to an array and calls the number in the id
+  var slotId = this.id.split("-")[1];
+  //task connects the id of saveBtn and text-area and allows to save to that specific textarea
   var task = $("#text-area-" + slotId).val();
+  //stringifys task and adds the corrisponding slot id
   localStorage.setItem(slotId, JSON.stringify(task)); //Set
 
   renderLastTask();
