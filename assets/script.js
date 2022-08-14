a32cbadf0f88620e67265dfb68a49eda910cc3cd
Gabe Vee <0gabevee0@gmail.com>
2022-08-14 12:55:54 -0600

    added color coding function for each hour of the day

--- assets/script.js
+++ assets/script.js
@@ -22,11 +22,31 @@ function renderLastTask() {
   }
 }
 
+function checkTime() {
+  //.each loops through
+  $(".time-block").each(function () {
+    var blockId = parseInt($(this).attr("id"));
+    var currentHour = moment().format("h");
+
+    if (currentHour > blockId) {
+      $(this).addClass("past");
+    } else if (currentHour == blockId) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+checkTime();
+
 //saves text area when clicked
 saveBtn.on("click", function () {
   console.log("I was clicked");
   //gets the id of the element this functions being applied to and splits the id
-  var slotId = this.id.split("-")[1];
+  var slotId = this.id.split("-")[1]; //splits save - i to an array and calls the number in the id
   console.log(slotId);
   //task is the id of text-area- slotId and gets the value of that specific text area
   //then sets the stringifyed task into local storage
