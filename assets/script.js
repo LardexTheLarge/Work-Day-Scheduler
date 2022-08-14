9a7ee2c4611984d91450a24a392b6eb3745c07e6
Gabe Vee <0gabevee0@gmail.com>
2022-08-14 00:47:52 -0600

    js update

--- assets/script.js
+++ assets/script.js
@@ -10,29 +10,26 @@ $("#currentDay").text(today);
 function renderLastTask() {
   //the value of timeslot gets taken from the local storage and put in the
   //correct time spot
-  for (let i = 1; i <= 12; i++) {
+  for (let i = 8; i <= 12; i++) {
     var keyValue = JSON.parse(localStorage.getItem(i));
     console.log(keyValue);
     $("#text-area-" + i).val(keyValue);
-
-    if (moment().format("h") == i) {
-      textArea.css("background-color", "redorange");
-    } else if (moment().format("h") < i) {
-      textArea.css("background-color", "gray");
-    } else if (moment().format("h") > i) {
-      textArea.css("background-color", "green");
-    }
-    console.log(moment().format("h") == i);
+  }
+  for (let i = 1; i <= 7; i++) {
+    var keyValue = JSON.parse(localStorage.getItem(i));
+    console.log(keyValue);
+    $("#text-area-" + i).val(keyValue);
   }
 }
 
 //saves text area when clicked
 saveBtn.on("click", function () {
   console.log("I was clicked");
-  //
+  //gets the id of the element this functions being applied to and splits the id
   var slotId = this.id.split("-")[1];
   console.log(slotId);
-
+  //task is the id of text-area- slotId and gets the value of that specific text area
+  //then sets the stringifyed task into local storage
   var task = $("#text-area-" + slotId).val();
   localStorage.setItem(slotId, JSON.stringify(task)); //Set
 
